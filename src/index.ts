2be05bc6416d8ef83122f81cc87aab8ac3bf0947
Gevik Babakhani <gevik@blendjs.com>
2019-08-11 22:11:20 +0200

    removed TypeBuilder from schemakit

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export * from "./database/PostgreSQLDatabase";
-export * from "./typescript/TypeBuilder";
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,3 @@
-import * as ejs from "ejs";
-import * as fs from "fs";
-import * as path from "path";
-
 /**
  * Logs the to console.
  *
@@ -12,35 +8,4 @@ export function log(message: string) {
     if (process.env.DEBUG) {
         console.log(message);
     }
-}
-
-/**
- * Renders a template
- *
- * @export
- * @param {string} name
- * @param {*} [data]
- * @returns {string}
- */
-export function renderTemplate(name: string, data?: any): string {
-    const file = path.join(__dirname, "..", "..", "templates", name.replace(/\//gim, path.sep));
-    const templates = fs.readFileSync(file).toString();
-    ejs.clearCache();
-    return ejs.render(templates, data || {});
-}
-
-/**
- * Converts tabs to spaces
- *
- * @export
- * @param {string} text
- * @returns {string}
- */
-export function tabsToSpaces(text: string): string {
-    return text
-        .split("\n")
-        .map(line => {
-            return line.replace(/\t/gim, "    ").trimRight();
-        })
-        .join("\n");
 }
